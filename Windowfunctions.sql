9e1eaf56ff61ab5d125588d95edfd91ebbfa3e0f
Pooja <79531660+pooja2434@users.noreply.github.com>
2024-12-29 14:33:31 +0530

    Update Windowfunctions.sql

--- Windowfunctions.sql
+++ Windowfunctions.sql
@@ -1,6 +1,6 @@
 Refer Table Creation.sql(https://github.com/pooja2434/SQL-Projects/blob/main/Table%20Creation.sql) to know about the table structure..
 
-  1. Rank
+  1. Rank()
 
 Ranks products based on price within each category, with ties receiving the same rank.
 PARTITION BY: Groups rows for window calculations
@@ -22,5 +22,16 @@ Similar to RANK() but does not skip ranks if there are ties.
     Price,
     Dense_rank() over(partition by CategoryID order by Price desc) as price_desc 
     from Products;
+
+3.ROW_NUMBER()
+  
+Assigns a unique row number to each product within each category, based on price.
+  
+select ProductID,
+    CategoryID,
+    Price,
+    Row_number() over(partition by CategoryID order by price desc) as rownumber 
+    from Products;
+
   
 
