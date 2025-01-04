f7dfd1f8c5ee710d875420885ce69ee841329770
Pooja <79531660+pooja2434@users.noreply.github.com>
2025-01-04 21:57:10 +0530

    Update Table Creation.sql

--- Table Creation.sql
+++ Table Creation.sql
@@ -23,18 +23,18 @@ Insert into Products  values(1, 	'Chais' ,	1 ,	1 ,	'10 boxes x 20 bags' 	,18 ),
 
 select * from Products;
 
---Creating table Employees 
+--Creating Table Employees
 
 Drop table Employees;
-
 create table Employees (EmployeeID INT primary key,LastName Varchar(20), FirstName Varchar(20),BirthDate date,
     Notes varchar(100));
-
 Insert into Employees values
-    (1, 'Davolio', 'Nancy', '12/8/1968','Education includes a BA in psychology from Colorado State University. She also completed The Art of the Cold Call. Nancy is a member of Toastmasters International' ),
-    (2,'Fuller','Andrew','2/19/1952','Andrew received his BTS commercial and a Ph.D. in international marketing from the University of Dallas. He is fluent in French and Italian and reads German. He joined the company as a sales representative, was promoted to sales manager and was then named vice president of sales. Andrew is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association' ),
-    (3 ,'Leverling','Janet' , '8/30/1963' ,'Janet has a BS degree in chemistry from Boston College. She has also completed a certificate program in food retailing management Janet was hired as a sales associate and was promoted to sales representative'),
-    (4,'Peacock', 'Margaret', '9/19/1958', 'Margaret holds a BA in English literature from Concordia College and an MA from the American Institute of Culinary Arts. She was temporarily assigned to the London office before returning to her permanent post in Seattle');
-    
-select * from Employees;
+    (1, 'Davolio', 'Nancy', '1968-12-08','Education includes a BA in psychology from Colorado State University. She also completed The Art of the Cold Call. Nancy is a member of Toastmasters International' ),
+    (2,'Fuller','Andrew','1952-02-19','Andrew received his BTS commercial and a Ph.D. in international marketing from the University of Dallas. He is fluent in French and Italian and reads German. He joined the company as a sales representative, was promoted to sales manager and was then named vice president of sales. Andrew is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association' ),
+    (3 ,'Leverling','Janet' , '1963-08-30' ,'Janet has a BS degree in chemistry from Boston College. She has also completed a certificate program in food retailing management Janet was hired as a sales associate and was promoted to sales representative'),
+    (4,'Peacock', 'Margaret', '1958-09-19', 'Margaret holds a BA in English literature from Concordia College and an MA from the American Institute of Culinary Arts. She was temporarily assigned to the London office before returning to her permanent post in Seattle');
+
+  
+  
+
 
