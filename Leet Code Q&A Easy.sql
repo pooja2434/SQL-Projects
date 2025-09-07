7fad048d2b1b9a3618cd525d1ebe731f629fe67d
Pooja <79531660+pooja2434@users.noreply.github.com>
2025-09-07 17:32:54 +0530

    Update Leet Code Q&A Easy.sql

--- Leet Code Q&A Easy.sql
+++ Leet Code Q&A Easy.sql
@@ -28,3 +28,20 @@ Customer table:
 +----+------+------------+
 
 Ans: select name from Customer where referee_id!=2 or referee_id IS NULL;
+
+3. A country is big if: it has an area of at least three million (i.e., 3000000 km2), or
+it has a population of at least twenty-five million (i.e., 25000000).
+Write a solution to find the name, population, and area of the big countries.Return the result table in any order.The result format is in the following example.
+Input: 
+World table:
++-------------+-----------+---------+------------+--------------+
+| name        | continent | area    | population | gdp          |
++-------------+-----------+---------+------------+--------------+
+| Afghanistan | Asia      | 652230  | 25500100   | 20343000000  |
+| Albania     | Europe    | 28748   | 2831741    | 12960000000  |
+| Algeria     | Africa    | 2381741 | 37100000   | 188681000000 |
+| Andorra     | Europe    | 468     | 78115      | 3712000000   |
+| Angola      | Africa    | 1246700 | 20609294   | 100990000000 |
++-------------+-----------+---------+------------+--------------+
+Ans: select name,population,area from World where area>=300000 and population>=25000000
+
